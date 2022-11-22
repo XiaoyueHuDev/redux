5eb77d18ca11bfeb40b9795895e9d1b6a8851a3d
xiaoyue hu <xiaoyuehu@xiaoyuedeMacBook-Pro.local>
2022-11-22 23:16:13 +0800

    add redux dev tool

--- examples/tree-view/src/index.js
+++ examples/tree-view/src/index.js
@@ -7,7 +7,7 @@ import generateTree from './generateTree'
 import Node from './containers/Node'
 
 const tree = generateTree()
-const store = createStore(reducer, tree)
+const store = createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 render(
     <Provider store={store}>
--- examples/tree-view/src/reducers/index.js
+++ examples/tree-view/src/reducers/index.js
@@ -1,4 +1,5 @@
 import { INCREMENT, ADD_CHILD, REMOVE_CHILD, CREATE_NODE, DELETE_NODE, ADD_FILE } from '../actions'
+import generateTree from '../generateTree';
 
 const childIds = (state, action) => {
   switch (action.type) {
@@ -64,7 +65,9 @@ const deleteMany = (state, ids) => {
   return state
 }
 
-export default (state = {}, action) => {
+const initialState = generateTree();
+
+export default (state = initialState, action) => {
   const { nodeId } = action
   if (typeof nodeId === 'undefined') {
     return state
